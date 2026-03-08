b9298bfb4c64b53ded9cdc282e05d6b195978344
ovictorqueiroz <victorbr442@gmail.com>
2026-03-07 23:50:13 -0300

    feat: preparando arquivo para recyclerview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -41,4 +43,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }

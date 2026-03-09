265012af5aad5ff937c885afac1ed6a5c78eb438
ovictorqueiroz <victorbr442@gmail.com>
2026-03-08 22:47:02 -0300

    feat: menu finalizado

--- app/src/main/java/br/com/etecia/ispec_app/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/ispec_app/MenuPrincipalActivity.java
@@ -8,12 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
-
-    ArrayList<MenuPrincipalModel> MenuPrincipalModel = new ArrayList<>();
-    int[] images = {R.drawable.ic_a_bell_duotone, R.drawable.ic_b_users_duotone, R.drawable.ic_c_calendar_dots_duotone, R.drawable.ic_d_file_text, R.drawable.ic_e_file_plus_duotone, R.drawable.ic_f_gear_six_duotone }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +21,6 @@ public class MenuPrincipalActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        setMenuPrincipalModel();
     }
 
-    private void setMenuPrincipalModel(){
-        String[] titulosMenu = getResources().getStringArray(R.array.titulos_menuPrincpal);
-
-        for (int i = 0; i > titulosMenu.length; i++){
-            MenuPrincipalModel.add(new MenuPrincipalModel(titulosMenu[i], images[i]));
-        }
-    }
 }
